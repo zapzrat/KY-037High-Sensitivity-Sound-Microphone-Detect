5425f3dca56b41233ef425554ab86e42d0f249ef
zapzrat <zapzrat@gmail.com>
2020-09-25 01:05:16 +0700

    Add files via upload

---
+++ Code/KY-037High_Sensitivity_Sound_Microphone_Detect/KY-037High_Sensitivity_Sound_Microphone_Detect.ino
@@ -0,0 +1,46 @@
+const int led = 13;
+int statusLed = 0;
+int checkAction = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Micro Project Thailand ~^.^~ ");
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  int sensorValue = analogRead(A0);
+//    Serial.println(sensorValue);
+
+
+  if (sensorValue >= 100) {
+   Serial.print("data : "); Serial.println(sensorValue);
+    checkAction++;
+    if (checkAction == 1) {
+//      Serial.println("dddddddd");
+      if (statusLed == 0) {
+        statusLed = 1;
+      }
+      else if (statusLed == 1) {
+        statusLed = 0;
+      }
+      delay(50);
+    }
+
+  }
+  else if (sensorValue < 100) {
+    checkAction = 0;
+  }
+
+  if (statusLed == 1) {
+    digitalWrite(led, HIGH);
+  }
+  else if (statusLed == 0) {
+    digitalWrite(led, LOW);
+  }
+
+
+
+
+
+}
